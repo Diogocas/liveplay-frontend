ef9b1d7a25dec5ee0a31895e50c60b9996960e65
Diogo Castro <diogocastrosilva2019@gmail.com>
2026-05-07 22:09:49 -0300

    substituir tudo pela dist correta

--- bundled-gta-mod/build/build-LivePlayGtaBridge.bat
+++ bundled-gta-mod/build/build-LivePlayGtaBridge.bat
@@ -4,6 +4,20 @@ set SCRIPT_DIR=%~dp0
 set GTA_DIR=%SCRIPT_DIR%..\..
 set SCRIPTS_DIR=%GTA_DIR%\scripts
 set CSC=%WINDIR%\Microsoft.NET\Framework64\v4.0.30319\csc.exe
-if not exist "%CSC%" set CSC=%WINDIR%\Microsoft.NET\Framework\v4.0.30319\csc.exe
-"%CSC%" /nologo /target:library /optimize+ /out:"%SCRIPTS_DIR%\LivePlayGtaBridge.dll" /reference:"%SCRIPTS_DIR%\ScriptHookVDotNet3.dll" "%SCRIPTS_DIR%\LivePlayGtaBridge.cs"
+set FRAMEWORK_DIR=%WINDIR%\Microsoft.NET\Framework64\v4.0.30319
+if not exist "%CSC%" (
+  set CSC=%WINDIR%\Microsoft.NET\Framework\v4.0.30319\csc.exe
+  set FRAMEWORK_DIR=%WINDIR%\Microsoft.NET\Framework\v4.0.30319
+)
+if not exist "%CSC%" (
+  echo Nao encontrei o compilador C# do .NET Framework 4.x.
+  pause
+  exit /b 1
+)
+if not exist "%GTA_DIR%\ScriptHookVDotNet3.dll" (
+  echo Nao encontrei ScriptHookVDotNet3.dll na raiz do GTA.
+  pause
+  exit /b 1
+)
+"%CSC%" /nologo /target:library /optimize+ /out:"%SCRIPTS_DIR%\LivePlayGtaBridge.dll" /reference:"%FRAMEWORK_DIR%\System.dll" /reference:"%GTA_DIR%\ScriptHookVDotNet3.dll" "%SCRIPTS_DIR%\LivePlayGtaBridge.3.cs"
 pause
